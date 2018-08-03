5e29b82a72eb0f35fe3ec93b319f52f38176faf1
aquino-d <aquino.d@hotmail.com>
2018-08-03 13:15:32 -0400

    Update Mad_lib.js

--- Mad_lib.js
+++ Mad_lib.js
@@ -1,10 +1,15 @@
+console.log("Start Program");
+
+// 4 Prompted User input
 var adjective = prompt("Please type an adjective (Uppercase)");
 var noun = prompt("Please type a noun ");
 var animal = prompt("Please type your favorite animal");
 var noise = prompt("Please type your favorite animal noise");
 
+//alerts user is done with input
 alert("You're done!");
 
+//User Input and String concatenation to create the variable message  end-result
 var message = adjective + " Macdonald had a ";
 message += noun + ", E-I-E-I-O </br> and on that ";
 message += noun + " he had a ";
@@ -17,3 +22,4 @@ message += adjective + " Macdonald had a ";
 message += noun + ", E-I-E-I-O.";
 
 document.write(message);
+console.log("End Program");
